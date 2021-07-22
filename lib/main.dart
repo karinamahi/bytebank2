fde79099275ba929ca4d49d9c6538d691089a48b
Karina Mayumi Hirata Bueno <CIANDT\karinab@lnb029135cps.ciandt.global>
2021-07-22 09:39:38 -0300

    project creation and image config

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ByteBankApp());
+}
+
+class ByteBankApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dashboard'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Image.asset('images/bytebank_logo.png'),
+            Container(
+              height: 120,
+              width: 100,
+              color: Colors.green,
+              child: Column(
+                children: <Widget>[
+                  Icon(Icons.people)
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
